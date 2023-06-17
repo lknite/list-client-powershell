87c26f4710b7e51437aba1b7ba2fd9b2fc5ff58f
lknite <lknite@aarr.xyz>
2023-06-17 12:03:51 -0600

    initial implementation of list client

--- list-client.ps1
+++ list-client.ps1
@@ -13,65 +13,111 @@ function Send-Message($WebSocket, $Message) {
     Write-Host "Sent message."
 }
 #>
+function IsContainer() {
+    $IsContainer = $false
 
-$Required = @(
-    "API_KEY"
-    "TEMPLATE"
-)
-foreach ($Req in $Required) {
-    if ([Environment]::GetEnvironmentVariable($Req).Length -eq 0) {
-        Write-Host "Abort, missing environment variable: $Req"
-        exit 1;
+    try {
+        # Attempt dns resolution of internal kubernetes service
+        $InternalServer = "kubernetes.default.svc";
+        [System.Net.Dns]::GetHostAddresses($InternalServer)
+
+        $IsContainer = $true
+    } catch { }
+
+    return $IsContainer
+}
+
+function CheckRequiredEnvironmentVariables($Required) {
+    foreach ($Req in $Required) {
+        if ([Environment]::GetEnvironmentVariable($Req).Length -eq 0) {
+            throw "Abort, missing environment variable: $Req"
+        }
     }
 }
 
-# Configure
-$Proto = "https"
-$GameServer = "gge.vc-non.k.home.net"
-$Server = "gge.vc-non.k.home.net"
-#$Proto = "http"
-#$GameServer = "gge-game.gge.svc"
-#$Server = "gge-gateway.gge.svc"
+# Verify required environment variables have been set
+$Required = @(
+    "API_KEY"
+)
+CheckRequiredEnvironmentVariables($Required)
 
-# Use internal websocket reference w/o tls
-if ($Proto -eq "http") {
-    $StreamURI = "ws://$($Server)/ws?api_key=$($Env:API_KEY)"
+# Configure target servers based on whether we are inside a container or not
+if (IsContainer) {
+    Write-Host "IsContainer: true"
+
+    $Proto = "http"
+    $ListServer = "list.list.svc"
+    $WebSocketServer = "list.list.svc"
+    $WebSocketUri = "ws://$($WebSocketServer)/ws?api_key=$($Env:API_KEY)"
 }
 else {
-    $StreamURI = "wss://$($Server)/ws?api_key=$($Env:API_KEY)"
+    Write-Host "IsContainer: false"
+
+    $Proto = "https"
+    $ListServer = "list.vc-non.k.home.net"
+    $WebSocketServer = "list.vc-non.k.home.net"
+    $WebSocketUri = "wss://$($WebSocketServer)/ws?api_key=$($Env:API_KEY)"
 }
 
+# Debug output
+Write-Host "API_KEY: $($Env:API_KEY)"
+Write-Host "ListServer: $($ListServer)"
+Write-Host "WebSocketServer: $($WebSocketUri)"
 
-
+# Declare
 $WebSocket = New-Object System.Net.WebSockets.ClientWebSocket
 $CancellationToken = New-Object System.Threading.CancellationToken
 $WebSocket.Options.UseDefaultCredentials = $true
 
 # Get connected
-Write-Host "StreamURI: $($StreamURI)"
-$Connection = $WebSocket.ConnectAsync($StreamURI, $CancellationToken)
-
-Write-Host "GameServer: $($GameServer)"
-Write-Host "API_KEY: $($Env:API_KEY)"
-Write-Host "TEMPLATE: $($Env:TEMPLATE)"
+$Connection = $WebSocket.ConnectAsync($WebSocketUri, $CancellationToken)
 
 # Establish connection to websocket
+Write-Host "Connecting to websocket ..." 
 While (-Not($Connection.IsCompleted)) { 
-    Start-Sleep -Milliseconds 500 
-    Write-Host "Still waiting to connect." 
+    Start-Sleep -Milliseconds 100 
 }
 
+# Debug output
 Write-Host "Connected."
+
+
+# Main
 try {
 
-# When playing a game, this is our player id
-$PlayerId = ""
+# track multiple lists
+$Lists = @{}
 
-# track multiple games
-$Game = @{}
+# check if there are already lists to process
+Write-Host "$($Proto)://$($ListServer)/list"
+$Items = Invoke-RestMethod `
+        -Method Get `
+        -Headers @{ "X-API-KEY" = $Env:API_KEY } `
+        "$($Proto)://$($ListServer)/list"
+
+# if so, acquire the state of each list to see if it is active
+foreach ($Item in $Items) {
+    Write-Host "list: $($Item)"
+
+    # get list details
+    $List = Invoke-RestMethod `
+            -Method Get `
+            -Headers @{ "X-API-KEY" = $Env:API_KEY } `
+            "$($Proto)://$($ListServer)/list?list=$($Item)"
+
+    # if active, add list to be processed
+    if ($List.state -eq "active") {
+        Write-Host "adding: $($List.state)"
+
+        # track this list
+        $Lists[$Item] = $List.state
+    }
+    else {
+        Write-Host "ignoring: $($List.state)"
+    }
+}
 
 # Loop long as connection exists
-"WebSocket.State: $($WebSocket.State)"
 while ($WebSocket.State -eq "Open") {
     # Reset recv array
     $Size = 1024
@@ -81,108 +127,42 @@ while ($WebSocket.State -eq "Open") {
     $Recv = New-Object System.ArraySegment[byte] -ArgumentList @(,$Array)
     $Connection = $WebSocket.ReceiveAsync($Recv, $CancellationToken)
     while (!$Connection.IsCompleted) { 
-        #Write-Host "Sleeping for 100 ms"
+        # While we wait for new data work on lists
+
+
+        # Avoid maxing the cpu if there are no lists to work on, or if processing is very quick
         Start-Sleep -Milliseconds 100 
     }
 
-    #
+    # We have new data from the server, convert from json
     $Result = ""
     $Result = [System.Text.Encoding]::ASCII.GetString($Recv.Array)
     $Result
     $o = $Result | ConvertFrom-Json
 
-    # debug output
+    # Debug output
     $o | ConvertTo-Json -Depth 3
 
-    switch ($o.type) {
-        # we've been invited to a game
-        "invite-new" {
-            # always accept an invite
-            Write-Host "$($Proto)://$($GameServer)/game/$($Env:TEMPLATE)/invite?game=$($o.game)&response=true"
+    switch ($o.event) {
+        # A new list for us to process
+        "active" {
+            Write-Host "adding: $($o.event)"
+
+            # track this list
+            $Lists[$o.list] = $o.event
+
+            <#
+            # get list details
+            Write-Host "$($Proto)://$($ListServer)/list?list=$($o.list)"
             $Result = Invoke-RestMethod `
-                    -Method PATCH `
+                    -Method Get `
                     -Headers @{ "X-API-KEY" = $Env:API_KEY } `
-                    "$($Proto)://$($GameServer)/game/$($Env:TEMPLATE)/invite?game=$($o.game)&response=true"
-
-            # track this game
-            $Game[$o.game] = New-Object -TypeName psobject -Property @{
-                PlayerId = 0
-            }
+                    "$($Proto)://$($ListServer)/list?list=$($o.list)"
+            #>
         }
-        "player-id" {
-            # we are being told our player id, indicates we are a player and not a watcher
-            $Game[$o.game].PlayerId = $o.data.id
-        }
-        # it's the next player's turn
-        "turn" {
-            # are we a watcher or a player?
-            if ($Game[$o.game].PlayerId -eq 0) {
-                # we are a watcher, don't try to make a move
-                break;
-            }
-            # is it our turn?
-            if ($o.data.id -eq $Game[$o.game].PlayerId) {
-                # yes, it is our turn, make a move
-                $method = "add"
-                $unit = ($Game[$o.game].PlayerId -eq 1) ? "x" : "y"
-                $grid = "main"
-                $level = "0"
-
-                # check existing items to determine available locations
-                Write-Host "$($Proto)://$($GameServer)/game/$($Env:TEMPLATE)/item?game=$($o.game)"
-                $Result = Invoke-RestMethod `
-                    -Method GET `
-                    -Headers @{ "X-API-KEY" = $Env:API_KEY } `
-                    "$($Proto)://$($GameServer)/game/$($Env:TEMPLATE)/item?game=$($o.game)"
-
-                # debug output
-                $Result | ConvertTo-Json -Depth 3
-
-                # number of existing items
-                Write-Host "Existing items: $($Result.Count)"
-
-                # look for available spot
-                $Candidates = @()
-                for ($y = 0; $y -le 2; $y ++) {
-                    for ($x = 0; $x -le 2; $x ++) {
-                        $Found = $false
-                        foreach ($Next in $Result) {
-                            if (($Next.gridy -eq $y) -and ($Next.gridx -eq $x)) {
-                                $Found = $true
-                                break
-                            }
-                        }
-                        if (!$Found) {
-                            $Candidates += [pscustomobject]@{
-                                gridy=$y
-                                gridx=$x
-                            }
-                        }
-                    }
-                }
-
-                # available plays
-                "Available plays: $($Candidates.Count)"
-                foreach ($Next in $Candidates) {
-                    "  gridy: $($Next.gridy), gridx: $($Next.gridx)"
-                }
-
-                # Chose available play at random
-                $Index = Get-Random -Maximum $Candidates.Count
-                $gridy = $Candidates[$Index].gridy
-                $gridx = $Candidates[$Index].gridx
-
-                # should always find a spot, because if cat the game will stop automatically
-                Write-Host "Selected play: gridy: $gridy, gridx: $gridx"
-                Write-Host "$($Proto)://$($GameServer)/game/$($Env:TEMPLATE)/play?game=$($o.game)&method=$($method)&unit=$($unit)&grid=$($grid)&gridy=$($gridy)&gridx=$($gridx)&level=$($level)"
-                $Result = Invoke-RestMethod `
-                    -Method POST `
-                    -Headers @{ "X-API-KEY" = $Env:API_KEY } `
-                    "$($Proto)://$($GameServer)/game/$($Env:TEMPLATE)/play?game=$($o.game)&method=$($method)&unit=$($unit)&grid=$($grid)&gridy=$($gridy)&gridx=$($gridx)&level=$($level)"
-            }
-        }
-        "game-complete" {
-            Write-Host "Nice."
+        # For now, by default ignore other list states
+        default {
+            Write-Host "ignoring: $($List.state)"
         }
     }
 }
